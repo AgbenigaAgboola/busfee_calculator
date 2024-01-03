83cb9a4b3ec292a1e0c6731801f33acf3ec7595f
Agboola Agbeniga <agboolaagbeniga@gmail.com>
2024-01-03 17:44:49 +0100

    changed the background

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 'use client'
 import Image from 'next/image'
 import BusServiceForm from './{components}/BusServiceForm'
+import backgroundImage from "@/public/background.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24" style={{
+      // use the src property of the image object
+      // backgroundImage: `url(${backgroundImage.src})`,
+
+      background:`linear-gradient(180deg, rgba(99,182,125,0.3) 6%, rgba(255,255,255,1) 26%, rgba(255,255,255,0.6) 79%, rgba(99,182,125,0.3) 98%), url(${backgroundImage.src})`,
+      // other styles
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      width: "100vw",
+      height: "100vh",
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+  
+    }}>
+      <p className='text-2xl font-bold'>Bus Service Fee Calculator</p>
 
       <BusServiceForm />
 
